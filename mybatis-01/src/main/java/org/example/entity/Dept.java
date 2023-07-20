37c70100eeb6e283aa3939b963d83b9f3189efaa
LTisme <284618025@qq.com>
2023-07-20 17:36:32 +0800

    已使用TestDept实现Collection高级查询嵌套，下一步：Collection高级结果嵌套

--- mybatis-01/src/main/java/org/example/entity/Dept.java
+++ mybatis-01/src/main/java/org/example/entity/Dept.java
@@ -5,12 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.List;
 
 /**
  * @Date: 2023/7/20
  * @Author: Administrator
  * @ClassName: Dept
- * @Description: comment here
+ * @Description: Dept和 Employee 都维护了对方的对象，双向维护
  */
 
 @Data
@@ -21,4 +22,6 @@ public class Dept implements Serializable {
     private static final Long serialVersionUID = 1L;
     private Integer id;
     private String deptName;
+
+    private List<Employee> employees;
 }
---
+++ mybatis-01/src/test/java/TestDept.java
@@ -0,0 +1,52 @@
+import lombok.extern.slf4j.Slf4j;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.example.entity.Dept;
+import org.example.entity.Employee;
+import org.example.mapper.DeptMapper;
+import org.example.mapper.EmployeeMapper;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * @Date: 2023/7/5
+ * @Author: Administrator
+ * @ClassName: TestMyBatis
+ * @Description:
+ */
+
+// 有了lombok就可以只用注解就可以替代日志：private static final Logger LOGGER = LoggerFactory.getLogger(CommonLogger.class);
+@Slf4j
+public class TestDept {
+    SqlSessionFactory sqlSessionFactory = null;
+
+    // 在执行Test之前都会先执行Before，这样可以把一些重复性的代码统一放到这里来处理
+    @Before
+    public void before() throws IOException {
+        // 从 XML 中构建 SqlSessionFactory，官网中还要求我们配置这个xml文件
+        String resource = "mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+    }
+
+    @Test
+    public void testSelect() {
+        try(SqlSession session = sqlSessionFactory.openSession()){
+            try {
+                DeptMapper mapper = session.getMapper(DeptMapper.class);
+                List<Dept> depts = mapper.select(null);
+                log.info("Depts are [{}]", depts);
+            } catch (Exception e){
+                log.error("发生了异常", e);
+                // 发生异常就回滚
+                session.rollback();
+            }
+        }
+    }
+}
