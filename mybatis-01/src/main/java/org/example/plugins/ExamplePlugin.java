914da412987994375fabf9077eaafca3efb5dc5d
LTisme <284618025@qq.com>
2023-08-02 15:13:09 +0800

    已使用拦截器插件为每一个sql语句尾部添加limit分页功能，MyBatis学习结束。

--- mybatis-01/src/main/java/org/example/plugins/ExamplePlugin.java
+++ mybatis-01/src/main/java/org/example/plugins/ExamplePlugin.java
@@ -1,7 +1,9 @@
 package org.example.plugins;
 
 import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
 import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.SqlSource;
 import org.apache.ibatis.plugin.Interceptor;
 import org.apache.ibatis.plugin.Intercepts;
 import org.apache.ibatis.plugin.Invocation;
@@ -9,6 +11,7 @@ import org.apache.ibatis.plugin.Signature;
 import org.apache.ibatis.session.ResultHandler;
 import org.apache.ibatis.session.RowBounds;
 
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -27,15 +30,35 @@ public class ExamplePlugin implements Interceptor {
 
     @Override
     public Object intercept(Invocation invocation) throws Throwable {
-        // implement pre processing if need
-        System.out.println("插件拦截之前");
-        Object returnObject = invocation.proceed();
-        // implement post processing if need
-        System.out.println("插件拦截之后");
-        // 这个属性是在配置文件中配置的
-        Object someProperty = properties.get("someProperty");
-        System.out.println("someProperty = " + someProperty);
-        return returnObject;
+        /**
+         * 使用拦截器为每一个sql语句尾部添加limit分页功能
+         */
+        // TODO: 2023/8/2 拿到原始 sql 语句，这就需要反射的功能了
+        /* 要知道sql语句是哪个参数，所有sql语句都在mapperStatement中被封装成一个类了，而这个类已经通过上述注解放进了invocation中 */
+        Object[] args = invocation.getArgs();
+        MappedStatement ms =  (MappedStatement)args[0];
+        /* 拿到了原始sql */
+        BoundSql boundSql = ms.getBoundSql(args[1]);
+        String sql = boundSql.getSql();
+
+        // TODO: 2023/8/2 开始改造 sql 语句
+        String newSql = sql + " limit 1, 10;";
+        /* 难点来了，ms没有set方法，那该如何将修改后的sql语句替换掉原来的sql语句呢？答案是再构建一个ms */
+        Executor executor = (Executor) invocation.getTarget();
+
+        SqlSource sqlSource = o -> new BoundSql(ms.getConfiguration(), newSql, null, args[1]);
+        MappedStatement newMappedStatement = new MappedStatement.Builder(
+                ms.getConfiguration(),
+                ms.getId() + "byPage",
+                sqlSource,
+                ms.getSqlCommandType())
+                .cache(ms.getCache())
+                .resultMaps(ms.getResultMaps())
+                .build();
+
+        List<Object> queryResult = executor.query(newMappedStatement, args[1], (RowBounds) args[2], (ResultHandler) args[3]);
+
+        return queryResult;
     }
 
     @Override
